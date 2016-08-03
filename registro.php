96bde63786b7ad13c8e02d0eb88152744eb35824
Ruben <ruben.agm02@gmail.com>
2016-08-02 22:43:38 -0500

    Se cambia el diseño del correo de registro

--- registro.php
+++ registro.php
@@ -25,73 +25,82 @@ $subject = 'Nuevo registro de: '
     . $_POST['nombre'];
 
 /*
+ * Los encabezados para que se vea mejor
+ */
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+
+
+/*
  * El contenido del mensaje
  */
 $txt = '
     <label>Datos personales</label>
     <table>
         <tr>
-            <td>Apellido Paterno</td>
+            <td style="font-weight: bold;">Apellido Paterno</td>
             <td>' . $_POST['apellidoPaterno'] . '</td>
         </tr>
         <tr>
-            <td>Apellido Materno</td>
+            <td style="font-weight: bold;">Apellido Materno</td>
             <td>' . $_POST['apellidoMaterno'] . '</td>
         </tr>
         <tr>
-            <td>Nombre/s</td>
+            <td style="font-weight: bold;">Nombre/s</td>
             <td>' . $_POST['nombre'] . '</td>
         </tr>
         <tr>
-            <td>Tel&eacute;fono fijo</td>
+            <td style="font-weight: bold;">Tel&eacute;fono fijo</td>
             <td>' . $_POST['telefono'] . '</td>
         </tr>
         <tr>
-            <td>Celular</td>
+            <td style="font-weight: bold;">Celular</td>
             <td>' . $_POST['celular'] . '</td>
         </tr>
         <tr>
-            <td>Correo electr&oacute;nico</td>
+            <td style="font-weight: bold;">Correo electr&oacute;nico</td>
             <td>' . $_POST['correo'] . '</td>
         </tr>
         <tr>
-            <td>Nombre empresa</td>
+            <td style="font-weight: bold;">Nombre empresa</td>
             <td>' . $_POST['nombreEmpresa'] . '</td>
         </tr>
         <tr>
-            <td>Raz&oacute;n social</td>
+            <td style="font-weight: bold;">Raz&oacute;n social</td>
             <td>' . $_POST['razonSocial'] . '</td>
         </tr>
         <tr>
-            <td>RFC/s</td>
+            <td style="font-weight: bold;">RFC/s</td>
             <td>' . $_POST['rfc'] . '</td>
         </tr>
         <tr>
-            <td>Calle</td>
+            <td style="font-weight: bold;">Calle</td>
             <td>' . $_POST['calle'] . '</td>
         </tr>
         <tr>
-            <td>N&uacute;mero Exterior</td>
+            <td style="font-weight: bold;">N&uacute;mero Exterior</td>
             <td>' . $_POST['numeroExterior'] . '</td>
         </tr>
         <tr>
-            <td>N&uacute;mero Interior</td>
+            <td style="font-weight: bold;">N&uacute;mero Interior</td>
             <td>' . $_POST['numeroInterior'] . '</td>
         </tr>
         <tr>
-            <td>C&oacute;digo postal</td>
+            <td style="font-weight: bold;">C&oacute;digo postal</td>
             <td>' . $_POST['codigoPostal'] . '</td>
         </tr>
         <tr>
-            <td>Colonia</td>
+            <td style="font-weight: bold;">Colonia</td>
             <td>' . $_POST['colonia'] . '</td>
         </tr>
         <tr>
-            <td>Municipio</td>
+            <td style="font-weight: bold;">Municipio</td>
             <td>' . $_POST['municipio'] . '</td>
         </tr>
         <tr>
-            <td>Estado</td>
+            <td style="font-weight: bold;">Estado</td>
             <td>' . $_POST['estado'] . '</td>
         </tr>
     </table>';
@@ -99,7 +108,7 @@ $txt = '
 
 
 foreach ($correos as $correo) {
-    mail($correo, $subject, $txt);
+    mail($correo, $subject, $txt, $headers);
 }
 
 echo json_encode(array("respuesta" => "correcto"));
